1f2ce67f98ccf376bc647463f805036b628d2542
dev-dongwon <dev.dongwonkim@gmail.com>
2022-11-13 06:12:03 +0900

    Feat: Add env for dev, nodemon, paths

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+// development에서는 ts를 쓰니까 요걸 타지 않는다.
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
