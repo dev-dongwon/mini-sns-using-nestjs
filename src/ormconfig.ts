115a8c7a4a81a8a838e933d0c2fb145157bb1b78
dev-dongwon <dev.dongwonkim@gmail.com>
2022-11-13 08:21:03 +0900

    Feat: Add database config with typeorm

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+const config: PostgresConnectionOptions = {
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "sns",
+  password: "123",
+  database: "sns",
+}
+
+export default config;
