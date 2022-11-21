317d236a9ea1e980cae78ef1dacd9789e542732d
dev-dongwon <dev.dongwonkim@gmail.com>
2022-11-21 13:45:23 +0900

    Feat: Add get single article route

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { ArticleService } from "./article.service";
 import { AuthGuard } from "@app/user/guards/auth.guard";
 import { User } from "@app/user/decorators/user.decorator";
@@ -17,6 +17,15 @@ export class ArticleController {
     @Body("article") createArticleDto: CreateArticleDto
   ): Promise<ArticleResponseInterface> {
     const article = await this.articleService.createArticle(currentUser, createArticleDto);
+
+    return this.articleService.buildArticleResponse(article);
+  }
+
+  @Get(":slug")
+  async getSingleArticle(
+    @Param("slug") slug:string
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.findBySlug(slug);
 
     return this.articleService.buildArticleResponse(article);
   }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -5,6 +5,7 @@ import { Repository } from "typeorm";
 import { CreateArticleDto } from "./dto/createArticle.dto";
 import { UserEntity } from "@app/user/user.entity";
 import { ArticleResponseInterface } from "./types/articleResponse.interface";
+import slugify from "slugify";
 
 @Injectable()
 export class ArticleService {
@@ -21,15 +22,25 @@ export class ArticleService {
       article.tagList = [];
     }
 
-    article.slug = "foo";
+    article.slug = this.getSlug(createArticleDto.title);
     article.author = currentUser;
 
     return await this.articleRepository.save(article);
+  }
+
+  async findBySlug(slug: string): Promise<ArticleEntity> {
+    return await this.articleRepository.findOneBy({ slug });
   }
 
   buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
     return {
       article
     }
+  }
+
+  private getSlug(title: string): string {
+    return slugify(title, {
+      lower: true
+    }) + "-" + ((Math.random() * Math.pow(36,6)) | 0).toString(36);
   }
 }
