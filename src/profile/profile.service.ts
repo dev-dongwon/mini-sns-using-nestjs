7c72e7222e102b6e0a8ac13ea3cb76662fc448d8
dev-dongwon <dev.dongwonkim@gmail.com>
2022-11-29 11:26:11 +0900

    Feat: Add get profile controller and service

---
+++ src/profile/profile.service.ts
@@ -0,0 +1,34 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ProfileType } from './types/profile.type';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '@app/user/user.entity';
+import { Repository } from 'typeorm';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+
+@Injectable()
+export class ProfileService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    return { ...user, following: false };
+  }
+
+  buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
+    delete profile.email;
+    return { profile };
+  }
+}
