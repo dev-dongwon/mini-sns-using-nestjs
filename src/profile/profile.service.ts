80cfa97cc52a2fdcc8e5bd55b29401252f9a683a
dev-dongwon <dev.dongwonkim@gmail.com>
2022-11-30 14:56:13 +0900

    Fix: fix the profile follow status

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -27,7 +27,11 @@ export class ProfileService {
       throw new HttpException('profile does not exist', HttpStatus.NOT_FOUND);
     }
 
-    return { ...user, following: false };
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    return { ...user, following: Boolean(follow) };
   }
 
   async followProfile(
