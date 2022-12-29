30f44595d1499055c8d88e976d71239dae821dea
dev-dongwon <dev.dongwonkim@gmail.com>
2022-12-30 02:28:25 +0900

    Feat: Add logger to visualize easier

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,34 @@ import { UserModule } from '@app/user/user.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { ArticleModule } from './article/article.module';
 import { ProfileModule } from './profile/profile.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule, ProfileModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TagModule,
+    UserModule,
+    ArticleModule,
+    ProfileModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AuthMiddleware).forRoutes({
-      path: "*",
+      path: '*',
       method: RequestMethod.ALL,
-    })
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { Logger } from 'nestjs-pino';
+
 // development에서는 ts를 쓰니까 요걸 타지 않는다.
 if (!process.env.IS_TS_NODE) {
   require('module-alias/register');
@@ -5,9 +7,12 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
